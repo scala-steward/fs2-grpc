042c86c5bd8bccaf71104a03fe0bf34d2a8ca92d
Scala Steward <me@scala-steward.org>
2021-07-03 22:20:39 +0200

    Update fs2-core to 3.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val grpc = scalapb.compiler.Version.grpcJavaVersion
     val scalaPb = scalapb.compiler.Version.scalapbVersion
 
-    val fs2 = "3.0.4"
+    val fs2 = "3.0.6"
     val catsEffect = "3.1.1"
     val ceMunit = "1.0.3"
 
